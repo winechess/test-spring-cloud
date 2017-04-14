942b8e15591fb6bf6fbffde7f45ad3952e874b76
Sergey Vinichenko <winechess@gmail.com>
2017-04-14 15:20:59 +0300

    Changes in gateway-service module:  - Change port of config server in bootstrap.yml  -

--- gateway-server/src/main/java/com/vinichenkosa/realty/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/vinichenkosa/realty/gatewayserver/GatewayServerApplication.java
@@ -13,10 +13,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
+@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
-@Slf4j
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
